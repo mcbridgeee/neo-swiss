f209d720d611dff86953c2629c395f59ffb28a50
mcbridgeee <bridgetmctiernan@gmail.com>
2025-11-16 19:32:58 -0500

    chore: major cleanup.  fixed broken build, removed Liquid conflicts, restored layouts, rebuilt main.css, and returned site to working state

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,22 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Make Eleventy copy your CSS folder to _site
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  // Add date filter for Nunjucks
+  eleventyConfig.addFilter("date", function(dateObj, format = "yyyy") {
+    return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
+  });
+
   return {
     dir: {
-      input: "src"
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/_layouts"
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "11ty.js"]
+    templateFormats: ["njk", "md", "html"]
   };
 };
