bfef99ce81704879347f7d94677a74d4ca0d41ba
mcbridgeee <bridgetmctiernan@gmail.com>
2025-11-17 04:50:01 -0500

    fix: set pathPrefix for github pages and copy css folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,11 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-
-  // Copy CSS file directly into output
-  eleventyConfig.addPassthroughCopy({
-    "src/css/main.css": "css/main.css"
-  });
+  // Copy the entire css folder to /css in the output
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 
   // Date filter
-  eleventyConfig.addFilter("date", function(dateObj, format = "yyyy") {
+  eleventyConfig.addFilter("date", function (dateObj, format = "yyyy") {
     return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
   });
 
@@ -18,6 +15,10 @@ module.exports = function(eleventyConfig) {
       includes: "_includes",
       layouts: "_includes/_layouts"
     },
+
+    // 🔥 THIS TELLS ELEVENTY YOUR SITE LIVES AT /neo-swiss/
+    pathPrefix: "/neo-swiss/",
+
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"]
